48a0afbb9a264b7e781acdd64e0d806452366fd0
JackKingLeon <jackleon415@gmail.com>
2025-07-31 10:38:38 -0700

    Big Solar Panel Update

--- src/pages/projects/TrackingSolar.jsx
+++ src/pages/projects/TrackingSolar.jsx
@@ -15,20 +15,71 @@ export default function StanderProject() {
         actuators, and coding any and all control systems and chips neccessary
         to track the sun.
       </p>
-    
-    <div className="flex justify-center">
-      <img
-        src="/images/circuitShematic.png"
-        alt="Tracking Solar Panel Circuit"
-        className="mb-6"
-      />
-    </div>
+
+      <div className="flex justify-center">
+        <img
+          src="/images/physicalSystem.jpg"
+          alt="High Level system Overview"
+          className="mb-6"
+        />
+      </div>
+
+      <p className="mb-4">
+        I designed the system to be easily scaleable with identacle code bases
+        in each arduino, a central control system (the raspberry pi in the
+        prototype), and a display that would give important
+        data/notifications/information at a glance
+      </p>
+
+      <div className="flex justify-center">
+        <img
+          src="/images/systemOverview.png"
+          alt="High Level system Overview"
+          className="mb-6"
+        />
+      </div>
+
+      <p className="mb-4">
+        The code for the capstone can be found on my github
+        <a
+          href="https://github.com/JackKingLeon/solarPanelCapstone"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          (here)
+        </a>
+        and the circuit is shown at the bottom of this page and can be
+        downloaded
+        <a
+          href="/circuit.png"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          here.
+        </a>
+        The full circuit consisted of current and voltage sensors to collect
+        data, a handful of step downs for voltage management, a raspberry pi, an
+        arduino, a couple relays so the raspberry pi can freely cut power to
+        systems that do not need it, components to facilitate communication
+        between the pi and sensors and the arduino and the pi, motor shields for
+        the actuators to plug into the arduino, and the actuators themselves.
+      </p>
 
       <ul className="list-disc list-inside mb-4 text-gray-700">
         <li>Custom CAD design using SolidWorks</li>
         <li>Fabricated with manual machining</li>
         <li>Tailored ergonomics and adjustability</li>
       </ul>
+
+      <div className="flex justify-center">
+        <img
+          src="/images/circuitShematic.png"
+          alt="Tracking Solar Panel Circuit"
+          className="mb-6"
+        />
+      </div>
     </main>
   );
 }
